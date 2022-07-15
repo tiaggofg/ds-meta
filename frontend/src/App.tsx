2765d51cda552e5a018daa4e74070c7420468929
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-07-14 23:50:43 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import NotificationButton from "./components/NotificationButton"
-import Header from "./components/header"
-import SaleCard from "./components/SaleCard"
+import NotificationButton from "./components/NotificationButton";
+import Header from "./components/header";
+import SaleCard from "./components/SaleCard";
 
 function App() {
   return (
@@ -17,4 +17,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,7 +1,7 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
-function NotificationButton(){  
+function NotificationButton() {  
     return (
         <div className="dsmeta-red-btn"> 
             <img src={icon} alt="Notificar" ></img>
@@ -9,4 +9,4 @@ function NotificationButton(){
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
